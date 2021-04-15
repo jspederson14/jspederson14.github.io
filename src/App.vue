0186bd7e40b28e79cee7be632b09989a93292557
Joe Pederson <joepederson@Joes-MacBook-Air.local>
2021-04-15 10:52:18 -0500

    add modded files

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/JoJo.jpg">
+  <HelloWorld msg= "Joe Pederson"/>
 </template>
 
 <script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,31 +2,27 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Hello. My name is Joe Pederson and I am current student at Knox College in Galesburg, Illinois. 
+      I am a sophmore planning on graduating in 2023 and majoring in Computer Science as well as a minor in Polical Science.
+      I am a member of the swim team at Knox as well as the captian of the water polo club team at Knox.
+      I am orginally from Roswell, Georgia. 
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Resume</h3>
+    <p>
+      (I don't have a proper resume so this is a place holder)
+    </p>
+    <h3>Past Projects</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://github.com/jspederson14/chess" target="_blank" rel="noopener">Chess</a></li>
+      <li><a href="https://github.com/jspederson14/policing_project" target="_blank" rel="noopener">Policing Project</a></li>
+      <li><a href="https://github.com/jspederson14/AsciiAdventure" target="_blank" rel="noopener">Ascii Adventure</a></li>
+      <li><a href="https://github.com/jspederson14/jspederson14.github.io" target="_blank" rel="noopener">This Site</a></li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h3>Contact Information</h3>
+    <p>
+      Email: jspederson@knox.edu &ensp;
+      Phone: 770-312-7238
+    </p> 
   </div>
 </template>
 
